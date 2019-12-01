c024cc8eb07837cf3c8d0be19941938d8b96a5b5
vique254 <victoronyango051@gmail.com>
2019-12-01 22:47:15 +0300

    create config file and add configurations

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    pass
+
+
+class ProdConfig:
+    pass
+class DevConfig:
+    DEBUG =True
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}   
