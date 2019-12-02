bb2a5618836ea3f733d5720e05df4d292c951af4
vique254 <victoronyango051@gmail.com>
2019-12-02 21:14:02 +0300

    create login and register files and add in login data

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,9 @@ def create_app(config_name):
     bootstrap.init_app(app)
 
    # Registering the blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from .forms import ReviewForm
-from ..models import Review
 
 
 # Views
 @main.route('/')
 def index():
+    return render_template('index.html')
