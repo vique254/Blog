35ff6db12a929a4f8a1c3a6c60785295efc4c582
vique254 <victoronyango051@gmail.com>
2019-12-03 08:18:59 +0300

    import sqlalchemy and create db instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
@@ -13,7 +15,7 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
-
+    db.init_app(app)
    # Registering the blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
