f2b99aea65bdefa2d7c457c6b80244b42ab54a85
vique254 <victoronyango051@gmail.com>
2019-12-03 23:28:52 +0300

    update files

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,6 +5,7 @@ from wtforms import ValidationError
 
 
 
+
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,8 @@ from . import main
 from flask_login import login_required, current_user
 from ..request import get_quotes
 from ..models import User
+from .forms import BlogForm,UpdateProfile
+from .. import db
 
 # Views
 @main.route('/')
@@ -36,3 +38,22 @@ def new_blog():
         
         
         return redirect(url_for('main.index'))
+    
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+        form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
