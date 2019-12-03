bb0e4f79be730bd9b80ea1153f9f5a88bf941dce
vique254 <victoronyango051@gmail.com>
2019-12-03 10:28:05 +0300

    import loginManager, and add secretKey

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from config import config_options
 
+
+# Instance of LoginManger and using its methods
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
@@ -12,10 +18,12 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    app.config ['SECRET_KEY']='Vique254'
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
    # Registering the blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
