5b117f4919c5b0b6aa577a6008a8e899dbc0f259
vique254 <victoronyango051@gmail.com>
2019-12-03 10:43:02 +0300

    import login_manager, UserMixin

--- app/models.py
+++ app/models.py
@@ -1,11 +1,21 @@
 from . import db
+from flask_login import UserMixin
+from . import login_manager
 
 #...
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
+    
     def __repr__(self):
         return f'User {self.username}'
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
