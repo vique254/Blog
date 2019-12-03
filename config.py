be8449e42d5f72c9c1dc241250663420caafab96
vique254 <victoronyango051@gmail.com>
2019-12-03 10:45:36 +0300

    update files

--- config.py
+++ config.py
@@ -1,17 +1,50 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    '''
+    General configuration parent class
+    '''
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY =os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:vique@localhost/blog'
+    # MAIL_SERVER='smtp.googlemail.com'
+    # MAIL_PORT= 587
+    # MAIL_USE_TLS= True
+    # MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
 
 
-class ProdConfig:
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings 
+    '''
     pass
-class DevConfig:
-    DEBUG =True
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
     
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:vique@localhost/blog'
+
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}   
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
