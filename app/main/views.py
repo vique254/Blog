aa593bf1cd4d07e08ea2ceb3a8a07de53dee6f30
vique254 <victoronyango051@gmail.com>
2019-12-03 10:39:51 +0300

    import current_user

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-
+from flask_login import login_required, current_user
 
 # Views
-@main.route('/')
+@main.route('/',methods=['GET','POST'])
 def index():
     return render_template('index.html')
