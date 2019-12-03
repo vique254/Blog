f268e5edd7d3d4739eac0102c56cc411f2b96c3d
vique254 <victoronyango051@gmail.com>
2019-12-03 23:17:32 +0300

    import abort

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required, current_user
 from ..request import get_quotes
@@ -9,6 +9,16 @@ from ..models import User
 def index():
     quotes=get_quotes()
     return render_template('index.html',quotes=quotes)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
 
 @main.route('/blog/new/', methods = ['GET','POST'])
 @login_required
