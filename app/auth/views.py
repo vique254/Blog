dd29c616bb48daca937100e959c57361ecc091e6
vique254 <victoronyango051@gmail.com>
2019-12-04 08:11:15 +0300

    import mail_message

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
-# from ..email import mail_message
+from ..email import mail_message
 from flask_login import login_user, login_required, logout_user
 
 #....
@@ -28,11 +28,11 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data)
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
         
-        # mail_message("Welcome to blog","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to blog","email/welcome_user",user.email,user=user)
           
         return redirect(url_for('auth.login'))
         title = "New Account"
