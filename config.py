ccdb7b35897bb6ec7319991aaf48efc40af08b14
vique254 <victoronyango051@gmail.com>
2019-12-04 07:49:17 +0300

    export mail username and password

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY =os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    
+    # email configs
     MAIL_SERVER='smtp.googlemail.com'
     MAIL_PORT= 587
     MAIL_USE_TLS= True
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-
+export MAIL_USERNAME='mzaesparta@gmail.com'
+export MAIL_PASSWORD= 'KE254254'
 export SECRET_KEY='Vique254'
 
 python3.6 manage.py server
