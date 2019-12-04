f2da493e149c78b54a21d749cf72965b4f32c88c
vique254 <victoronyango051@gmail.com>
2019-12-04 08:00:02 +0300

    create sender email and mail_message function

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_uploads import UploadSet,configure_uploads,IMAGES
-
+from flask_mail import Mail 
 
 # Instance of LoginManger and using its methods
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
+mail = Mail()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
@@ -24,6 +25,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
     login_manager.init_app(app)
    # Registering the blueprint
     from .auth import auth as auth_blueprint
